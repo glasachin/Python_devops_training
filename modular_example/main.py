fa9f89f42308e45bc37a238cbd71f36dfdea5a75
glasachin <glasachin@yahoo.co.in>
2025-03-26 18:26:20 +0530

    update

--- modular_example/main.py
+++ modular_example/main.py
@@ -6,20 +6,37 @@ from app import appObj
 import asyncio
 import utility
 
+import multiprocessing
+import multiprocess_test.subProcess as sp
+
 if __name__ == '__main__':
-    wifi_obj = WiFi()
-    print('From Main: ', wifiObj.a)
-    print('From Main: ', wifiObj.b)
-    wifiObj.inc_b()
-    print('From Main: ', wifiObj.b)
-    modemObj.modemPrint()
-    print('From Main: ', wifi_obj.b)
-    wifiObj.path()
+    # wifi_obj = WiFi()
+    # print('From Main: ', wifiObj.a)
+    # print('From Main: ', wifiObj.b)
+    # wifiObj.inc_b()
+    # print('From Main: ', wifiObj.b)
+    # modemObj.modemPrint()
+    # print('From Main: ', wifi_obj.b)
+    # wifiObj.path()
 
-    print('Global Variable: ', utility.a)
-    modemObj.updateUtility()
-    print('Global Variable: ', utility.a)
-    utility.appPath = str(Path(__file__).parents)
-    print('App Path: ', utility.appPath)
+    # print('Global Variable: ', utility.a)
+    # modemObj.updateUtility()
+    # print('Global Variable: ', utility.a)
+    # utility.appPath = str(Path(__file__).parents)
+    # print('App Path: ', utility.appPath)
 
-    asyncio.run(appObj.startMainApp())
+    # asyncio.run(appObj.startMainApp())
+
+    # Multiprocessing test
+    queue = multiprocessing.Queue()
+    multiprocessing.Process(target=sp.queueTest, args=(queue,)).start()
+
+    while True:
+        try:
+            val = queue.get(timeout=2)
+        except Exception as e:
+            val = None
+
+        if val is None:
+            print(True)
+        print('data: ', val)
---
+++ modular_example/multiprocess_test/subProcess.py
@@ -0,0 +1,8 @@
+import random
+import time
+def queueTest(queue):
+    
+    while(1):
+        # queue.put((random.randint(1,10),random.randint(1,10)))
+        # time.sleep(0.5)
+        time.sleep(1)
